c6010a51735d14739ba3256dadfda24a83752609
Raphael <raphaklaus@gmail.com>
2020-03-20 17:13:47 +0100

    add package info

---
+++ .elixir_ls/build/lib/app/ebin/app.app
@@ -0,0 +1,6 @@
+{application,app,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"app"},
+              {modules,['Elixir.App']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule App.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +24,18 @@ defmodule App.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "elixir-github-actions",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/raphaklaus/elixir-github-actions"}
     ]
   end
 end
